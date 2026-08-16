84283cef932c4bc9b3e73372cab16a8799de7a36
Nandalal Sivadas <nandalalsivadas@Nandas-MacBook-Air.local>
2026-08-17 00:34:44 +0530

    Use supplied Viral logo asset

--- app/components/ViralMark.tsx
+++ app/components/ViralMark.tsx
@@ -1,10 +1,3 @@
 export function ViralMark({ small = false }: { small?: boolean }) {
-  return (
-    <span className={`viral-mark${small ? " viral-mark-small" : ""}`} aria-hidden="true">
-      <i className="viral-block viral-block-a" />
-      <i className="viral-block viral-block-b" />
-      <i className="viral-block viral-block-c" />
-      <i className="viral-block viral-block-d" />
-    </span>
-  );
+  return <img className={`viral-mark${small ? " viral-mark-small" : ""}`} src="/viral-logo-exact.png" alt="" aria-hidden="true" />;
 }
